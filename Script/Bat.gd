be160b7b73429de0c75ab2898f5ae90d3f7ac4f0
timilyo80 <78914833+timilyo80@users.noreply.github.com>
2023-06-07 13:34:47 -0400

    Enemy Stay
    Janky system that make enemies stay dead after reloading the same level. (TODO: make enemies ID auto-increment, somehow)

--- Script/Bat.gd
+++ Script/Bat.gd
@@ -5,6 +5,7 @@ const DeathEffect = preload("res://BatDeathEffect.tscn")
 export var ACCELERATION = 300
 export var MAX_SPEED = 50
 export var FRICTION = 200
+export var ID = 0
 
 enum {
 	IDLE,
@@ -16,12 +17,17 @@ var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
 var state = CHASE
+var permanence = Permanence
 
 onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetection
 onready var hurtbox = $Hurthbox
 onready var softColision = $SoftCollision
+
+func _ready():
+	if permanence.is_Exist(get_tree().current_scene.name, ID) != -1:
+		queue_free()
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -62,3 +68,4 @@ func _on_Stats_no_health():
 	var deathEffect = DeathEffect.instance()
 	get_parent().add_child(deathEffect)
 	deathEffect.global_position = global_position
+	permanence.unexist(get_tree().current_scene.name, ID)
--- Script/Permanence.gd
+++ Script/Permanence.gd
@@ -2,3 +2,17 @@ extends Node
 
 var LevelposX = 160
 var LevelposY = 88
+
+var unexistDict = {}
+
+func unexist(Level, ID):
+	if !unexistDict.has(Level):
+		unexistDict[Level] = []
+		
+	unexistDict[Level].append(ID)
+
+func is_Exist(Level, ID):
+	if unexistDict.has(Level):
+		return unexistDict[Level].find(ID)
+	else:
+		return -1
